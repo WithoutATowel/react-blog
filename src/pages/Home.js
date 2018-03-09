4e58bd10e1b32d2d7791b8d2d2d0504e673de2dd
Brett <brettspencr@gmail.com>
2018-03-09 15:53:06 -0800

    Yeaaaa

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,67 @@
-import React from 'react';
+import React, {Component} from 'react';
+const axios = require('axios');
 
-const Home = () => {
-    return (
-        <div className='container'>
-            <h1>This is home</h1>
-            <p>Thanks for visiting! You're the only one who's ever looked at this site!</p>
-        </div>
-    )
+class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            shakespeare: ''
+        }
+    }
+
+    componentDidMount() {
+
+        let poemApi = 'http://ShakeItSpeare.com/api/poem'
+        
+        // our fetch of that poemApi
+        // fetch(poemApi)
+        //     .then((response) => {
+        //         return response.json();
+        //     })
+        //     .then((json) => {
+        //         base.setState({ shakespeare: json.poem });
+        //     })
+        //     .catch((error) => {
+        //         console.log('An error occurred while parsing:', error);
+        //     });
+
+        axios.get(poemApi)
+            .then((response) => {
+                console.log(response);
+                this.setState({ shakespeare: response.data.poem });
+            })
+            .catch((error) => {
+                console.log('An error occurred while parsing:', error);
+            })
+
+            // axios({
+            //     method: 'GET',
+            //     url: poemApi
+            // })
+            // .then((response) => {
+            //     this.setState({ shakespeare: response.data.poem })
+            // })
+    }
+
+    render() {
+
+        let poetry = ''
+
+        if(this.state.shakespeare) {
+            poetry = this.state.shakespeare;
+        } else {
+            poetry = "Loading..."
+        }
+
+        return (
+            <div className='container'>
+                <h1>This is my homepage</h1>
+                <h2>My favorite Shakespeare poem:</h2>
+                <p>{poetry}</p>
+            </div>
+        )
+        
+    }
 }
 
 export default Home;
